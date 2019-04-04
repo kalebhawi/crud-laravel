f3fb9e5aca8245dfb8aaa048e8f8ca763baf8bf7
KalebHawi <klblps@yahoo.com.br>
2019-04-04 13:34:56 -0300

    auth verification in clientController added and about us view created

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,29 @@
 @extends('layouts.app')
 @section('content')
+<div class="col-md-12">
+    <div class="col-md-12" align="center">
+        <h1>About us</h1>
+    </div>
 
+    <div class="row">
+        <div class="col-md-4" align="center">
 
-    About Us
+        </div>
+        <div class="col-md-5">
+            <ul>
+                <li><strong>Name:</strong> {{--TODO: criar modo de inserir dados pelo config--}}</li>
+                <li><strong>Student:</strong> </li>
+                <li><strong>Educational institution:</strong> </li>
+                <li><strong>Period:</strong> </li>
+            </ul>
+        </div>
+
+    </div>
+
+    <footer>
+        <p><strong>Contact:</strong></p>
+    </footer>
+
+</div>
 
 @endsection
